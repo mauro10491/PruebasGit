40554cb178ad32720b6da4f27896820386c9b6b3
Mauricio <mauricio.alzate13701@udecaldas.edu.co>
2023-03-31 15:26:39 -0500

    Prueba

---
+++ pruebasgit1/src/main/java/com/mycompany/pruebasgit1/Pruebasgit1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebasgit1;
+
+/**
+ *
+ * @author mauri
+ */
+public class Pruebasgit1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
