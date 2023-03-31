156ef6682205483974f51ace929516a06d82ddf8
Mauricio <mauricio.alzate13701@udecaldas.edu.co>
2023-03-31 15:27:40 -0500

    cambio 2

--- pruebasgit1/src/main/java/com/mycompany/pruebasgit1/Pruebasgit1.java
+++ pruebasgit1/src/main/java/com/mycompany/pruebasgit1/Pruebasgit1.java
@@ -11,6 +11,6 @@ package com.mycompany.pruebasgit1;
 public class Pruebasgit1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Mauricio Alzate Ocampo");
     }
 }
